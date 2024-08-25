e8a11e673e5ad5f4833313ddd749bfbf893a8d3e
ArtZenuni <71331084+ArtZenuni@users.noreply.github.com>
2024-08-25 12:08:16 +0100

    Delete assets/weekly-project/week_03_shopping directory

--- assets/weekly-project/week_03_shopping/scripts/burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.getElementById('menu-icon');
-    const menu = document.getElementById('menu');
-
-    menuIcon.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
-});
