4ad4d1d0a63735018a18c46e9ef6bb75366b7de7
ArtZenuni <71331084+ArtZenuni@users.noreply.github.com>
2025-01-07 14:35:01 +0000

    Delete assets/projects/carnaval_de_quebec directory

--- assets/projects/carnaval_de_quebec/scripts/burger.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const menu = document.querySelector('.menu');
-
-    burgerMenu.classList.toggle('open');
-    menu.classList.toggle('open');
-}
